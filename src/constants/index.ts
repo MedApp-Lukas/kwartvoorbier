faca45f1cdf83966c145279ccaaf7a55bc21fbdd
Rens Hendrix <renshendrix@Renss-MacBook-Air.local>
2025-10-09 15:46:47 +0200

    Test

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@
 export const ORDER_START_HOUR = 15;
 export const ORDER_START_MINUTE = 35;
 export const ORDER_END_HOUR = 16;
-export const ORDER_END_MINUTE = 0;
+export const ORDER_END_MINUTE = 15;
