7382f1a24d1fa7ea490886036c76f91ffb1712dd
Rens Hendrix <renshendrix@Renss-MacBook-Air.local>
2025-10-10 07:36:47 +0200

    Reverted changes order time

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 
-export const ORDER_START_HOUR = 6;
+export const ORDER_START_HOUR = 15;
 export const ORDER_START_MINUTE = 45;
 export const ORDER_END_HOUR = 16;
 export const ORDER_END_MINUTE = 0;
